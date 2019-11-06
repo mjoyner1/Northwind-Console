7f19122a92cc22f467a8ce162997083705fc212e
Michael Joyner <mjoyner1@my.wctc.edu>
2019-11-06 09:51:38 -0600

    Option 1 Display Categories

--- Northwind-Console/Program.cs
+++ Northwind-Console/Program.cs
@@ -10,18 +10,30 @@ namespace NorthwindConsole
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             logger.Info("Program started");
             try
             {
                 string choice;
                 do
                 {
-                    Console.Clear();
                     Console.WriteLine("1) Display Categories");
                     Console.WriteLine("2) Add Category");
                     Console.WriteLine("\"q\" to quit");
                     choice = Console.ReadLine();
+                    Console.Clear();
+                    logger.Info($"Option {choice} selected");
+                    if (choice == "1")
+                    {
+                        var db = new NorthwindContext();
+                        var query = db.Categories.OrderBy(p => p.CategoryName);
+
+                        Console.WriteLine($"{query.Count()} records returned");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine($"{item.CategoryName} - {item.Description}");
+                        }
+                    }
+                    Console.WriteLine();
 
                 } while (choice.ToLower() != "q");
             }
